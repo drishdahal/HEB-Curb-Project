71bb6e49f529c52d7666e21a1d7be816631061fd
drishdahal <dahaldrishya@gmail.com>
2020-04-14 18:30:24 -0500

    Update HEBMain.py
    Added up to change store before zoom meeting

--- HEBMain.py
+++ HEBMain.py
@@ -10,5 +10,8 @@ storename=[]
 timeslot=[]
 
 driver.find_element_by_xpath('/html/body/rel/div[2]/div/div/bootstrap/div/div[1]/div/div/footer/button').click() #click OK on HEB initial warning
+driver.find_element_by_xpath('/html/body/rel/div[2]/div/div/div[2]/header/div[2]/div[2]/div[2]/div[1]/div/bootstrap/div/button').click() #click on reserve time slot
+driver.find_element_by_xpath('/html/body/rel/div[2]/div/div/div[2]/header/div[2]/div[2]/div[2]/div[1]/div/bootstrap/bootstrap/div[1]/div[1]/div/div/div/div/div[2]/div[1]/div[1]/button') #click on change store
+
 # df = pd.DataFrame({'Store Name':storename,'timeslot':timeslot})
 # df.to_csv('products.csv', index=False, encoding='utf-8')
